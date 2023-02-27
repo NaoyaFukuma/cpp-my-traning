5d794857f3c6315b0afff561d5b96ad8ef40c09e
nfukuma <fukumanaoya@gmail.com>
2023-02-27 02:08:41 +0000

    current time output add

--- hellow world/main.cpp
+++ hellow world/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <chrono>
 
 int main(){
 	std::cout << "Hellow World!!" << std::endl;
+	std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+	std::time_t t = std::chrono::system_clock::to_time_t(now);
+	std::cout << std::ctime(&t) << std::endl;
 }
